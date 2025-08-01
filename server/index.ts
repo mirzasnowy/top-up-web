40c11a008b0822785586c90a45e3c3b3d757028c
Muhammad Mirza <2210631170085@student.unsika.ac.id>
2025-08-01 14:38:59 +0700

    try to fix

--- server/index.ts
+++ server/index.ts
@@ -13,13 +13,13 @@ export function createServer() {
   app.use(express.urlencoded({ extended: true }));
 
   // Example API routes
-  app.get("/ping", (_req, res) => {
+  app.get("/api/ping", (_req, res) => {
     const ping = process.env.PING_MESSAGE ?? "ping";
     res.json({ message: ping });
   });
 
-  app.get("/demo", handleDemo);
-  app.use("/midtrans", midtransRouter);
+  app.get("/api/demo", handleDemo);
+  app.use("/api/midtrans", midtransRouter);
 
   return app;
 }
